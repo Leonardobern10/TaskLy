a6d134de6d04c0dfba9800dd1acc906ab826bc4c
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2026-01-06 16:18:35 -0300

    "config: new"

---
+++ packages/types/dist/MessagePatterns.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MessagePatterns = void 0;
+var MessagePatterns;
+(function (MessagePatterns) {
+    MessagePatterns["AUTH_LOGIN"] = "auth.login";
+})(MessagePatterns || (exports.MessagePatterns = MessagePatterns = {}));
---
+++ packages/types/src/MessagePatterns.ts
@@ -0,0 +1,3 @@
+export enum MessagePatterns {
+  AUTH_LOGIN = "auth.login",
+}
