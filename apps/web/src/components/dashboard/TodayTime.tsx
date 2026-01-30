92a6b90235b66f2ea6eabd45d03141eb938200dc
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2026-01-30 15:59:38 -0300

    style: Data e hora removidos

--- apps/web/src/components/dashboard/TodayTime.tsx
+++ apps/web/src/components/dashboard/TodayTime.tsx
@@ -3,29 +3,31 @@ import Clock from "./Clock";
 import { motion } from "motion/react";
 
 const styles = {
-  container: "flex flex-row items-center justify-between gap-x-5 font-mono",
-  date: "text-muted-foreground text-lg",
-  day: "text-neutral-700/80 text-4xl",
-  dot: "w-1 h-1 bg-black/50 rounded-full self-center",
+  container: "flex flex-row items-end justify-start gap-5 font-sans",
+  date: "text-muted-foreground text-md",
+  day: "text-neutral-700/80 text-xl",
+};
+
+const animateOptions = {
+  initial: { x: -300 },
+  animate: { x: 0 },
+  transition: {
+    duration: 1,
+    scale: {
+      visutalDuration: 0.4,
+      bounce: 0.5,
+    },
+  },
 };
 
 export default function TodayTime() {
   const { formattedDate, formattedDay } = useDashboard();
   return (
-    <motion.div
-      initial={{ x: -300 }}
-      animate={{ x: 0 }}
-      transition={{
-        duration: 1,
-        scale: { visualDuration: 0.4, bounce: 0.5 },
-      }}
-    >
+    <motion.div {...animateOptions}>
       <div className={styles.container}>
         <p className={styles.day}>{formattedDay}</p>
-        <div className={styles.dot} />
         <p className={styles.date}>{formattedDate}</p>
       </div>
-      <Clock />
     </motion.div>
   );
 }
